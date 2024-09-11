a908a60bb17a1cb46eb15a01fef773ef21ab9d22
MizuleGPT <141879282+MizuleGPT@users.noreply.github.com>
2024-09-11 15:34:00 -0700

    Smaller model and less data in train_gpt2.py

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -7,18 +7,18 @@ wandb_project = 'owt'
 wandb_run_name='gpt2-124M'
 
 # these make the total batch size be ~0.5M
-# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 12
-block_size = 1024
-gradient_accumulation_steps = 5 * 8
+# 8 batch size * 256 block size * 4 gradaccum * 1 GPU = ?
+batch_size = 8
+block_size = 256
+gradient_accumulation_steps = 4
 
-# this makes total number of tokens be 300B
-max_iters = 600000
-lr_decay_iters = 600000
+# this makes total number of tokens be ?
+max_iters = 1024
+lr_decay_iters = 1024
 
 # eval stuff
-eval_interval = 1000
-eval_iters = 200
+eval_interval = 10
+eval_iters = 2
 log_interval = 10
 
 # weight decay
